b45dc9d2ecd7db5ca3311fcc3e5428fed25d9851
ArunKumar <arunkumar.e@outlook.com>
2024-11-28 11:04:58 +0530

    Add project files.

---
+++ Immo.MVC.Day2/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Immo.MVC.Day2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Immo.MVC.Day2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Products = GetProducts();
+            ViewData["Products"] = GetProducts();
+            return View();
+        }
+
+        [NonAction]
+        public IEnumerable<Product> GetProducts()
+        {
+            return new List<Product>
+            {
+                new Product { Id = 1, ProductName="Laptop", Price= 500000.5M, Quantity=100 },
+                new () { Id = 2, ProductName="Water Bottle", Price= 850, Quantity=252 },
+                new () { Id = 3, ProductName="Watch", Price= 7599.99M, Quantity=15 },
+                new() { Id = 4, ProductName="Mobile Phone", Price= 19999.99M, Quantity=50 },
+             };
+        }
+    }
+}
